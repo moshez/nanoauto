8e1c397c1dc68eb32158c5ad5436f5ef83ca0374
Moshe Zadka <zadka.moshe@gmail.com>
2015-03-22 20:05:07 -0700

    it works

---
+++ nanoauto/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.0.1-dev'
+
+metadata = dict(
+    name='nanoauto',
+    version=__version__,
+    description='Web-based editor',
+    long_description="""\
+nanoauto: write-only web editor
+""",
+    author='Moshe Zadka',
+    author_email='zadka.moshe@gmail.com',
+    license='MIT',
+    copyright='2015',
+)
--- setup.py
+++ setup.py
@@ -10,35 +10,10 @@ import setuptools
 
 import nanoauto as module
 
-class OptionLessCommand(cmd.Command):
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-class ToxTestCommand(OptionLessCommand):
-
-    def run(self):
-        subprocess.check_call(['tox'])
-
-class All(OptionLessCommand):
-
-    def run(self):
-        self.run_command('test')
-        self.run_command('bdist_wheel')
-        self.run_command('build_sphinx')
-
 setuptools.setup(
-    ## url='https://github.com/moshez/ncolony',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Environment :: No Input/Output (Daemon)',
         'Framework :: Twisted',
-        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
         'Topic :: System',
@@ -46,12 +21,10 @@ setuptools.setup(
         'Programming Language :: Python :: 2.7',
     ],
     keywords='editor web twisted',
-    packages=[module.__name__],
-    install_requires=['Twisted', 'klein'],
+    packages=[module.__name__, 'twisted.plugins'],
+    install_requires=['Twisted', 'ncolony', 'klein'],
     extras_require = {
-        'test': ['tox'],
-        'dev': ['wheel', 'sphinx'],
+        'dev': ['wheel'],
     },
-    cmdclass=dict(test=ToxTestCommand, all=All),
     **module.metadata
 )
